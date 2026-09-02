63aea42f54b861dcef4a9a191e49b8e39dd3533e
hikmat-sfrzdh <hikmatsafarzadeh@gmail.com>
2026-09-02 10:32:32 +0400

    cors

--- server/src/routes/app.js
+++ server/src/routes/app.js
@@ -29,8 +29,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
-
 app.use(express.json())
 app.use("/api/cars", carRouter)
 app.use("/api/auth", authRouter)
